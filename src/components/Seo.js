0d18951e5b3f4d933df342e9b9fd1b665769279d
NQphuoc <nguyenqp97@gmail.com>
2020-05-30 11:34:16 +0700

    fix cdn

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -3,7 +3,23 @@ import { Helmet } from "react-helmet"
 export default function Seo(props) {
   return (
     <>
-      <Helmet title={props.title} defer={false} />
+      <Helmet title={props.title} defer={false}>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        ></link>
+      </Helmet>
     </>
   )
 }
